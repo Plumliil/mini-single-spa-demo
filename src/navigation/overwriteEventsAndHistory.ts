05bf12a206bd47708042576f048bddb3d9cc9cf2
plumliil_li <plumliil.li@elitesland.com>
2023-09-13 23:29:57 +0800

    feat(navigation) : 浏览器修改历史记录栈方法重写

---
+++ src/navigation/overwriteEventsAndHistory.ts
@@ -0,0 +1,31 @@
+import { loadApps } from 'src/application/apps'
+
+const originPushState = window.history.pushState
+const originReplaceState = window.history.replaceState
+
+export default function overwriteEventAndHistory() {
+  window.history.pushState = function (
+    data: any,
+    unused: string,
+    url?: string | URL | null | undefined
+  ): void {
+    const result = originPushState.call(this, data, unused, url)
+    loadApps('pushState')
+    return result
+  }
+  window.history.replaceState = function (
+    data: any,
+    unused: string,
+    url?: string | URL | null | undefined
+  ) {
+    const result = originReplaceState.call(this, data, unused, url)
+    loadApps('replaceState')
+    return result
+  }
+  window.addEventListener('popstate', () => {
+    console.log('popstate')
+  })
+  window.addEventListener('hashChange', () => {
+    console.log('hashChange')
+  })
+}
