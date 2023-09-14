2614591f550651efe05692cee11969111f29b1cd
plumliil_li <plumliil.li@elitesland.com>
2023-09-14 23:39:58 +0800

    feat(navigation) : Improve apps load methods

--- src/application/apps.ts
+++ src/application/apps.ts
@@ -1,27 +1,48 @@
+import bootstrapApp from 'src/lifecycle/bootstrap'
+import mountApp from 'src/lifecycle/mounted'
+import unmountApp from 'src/lifecycle/unMounted'
 import { AppStatus, Application } from 'src/type'
 
 export let apps: Application[] = []
 
-export function loadApps(str: string) {
+export async function loadApps(str: string) {
+  // 对已注册的应用 进行初始化 将其状态转化为BOOTSRTAP
   const toBootstrapApp = getAppByStatus(AppStatus.BEFORE_BOOTSTRAP)
+  // 异步执行
+  await Promise.all(toBootstrapApp.map(bootstrapApp))
+  // 卸载应用 activeRule返回false且应用当前状态为mount 则对该应用进行卸载
+  // 是当前路由且状态为MOUNTED不做任何操作
+  // 否则进行卸载
   const toUnMountApp = getAppByStatus(AppStatus.MOUNTED)
-  console.log('loadApps', { toBootstrapApp, toUnMountApp })
+
+  await Promise.all(toUnMountApp.map(unmountApp))
+  const toMountApp = [
+    ...getAppByStatus(AppStatus.UNMOUNTED),
+    ...getAppByStatus(AppStatus.BOOTSTRAPPED),
+  ]
+
+  await Promise.all(toMountApp.map(mountApp))
+  console.log('app', apps)
 }
 
 function getAppByStatus(status: AppStatus) {
   const result: Application[] = []
   apps.forEach((app) => {
     // 启动或者挂载
+    // 当当前路由满足路由规则条件且传入状态等于当前app状态
     if (isActive(app) && app.status === status) {
+      // 待挂载
       switch (app.status) {
-        case AppStatus.BEFORE_BOOTSTRAP:
-        case AppStatus.BOOTSTRAPPED:
-        case AppStatus.UNMOUNTED:
+        case AppStatus.BEFORE_BOOTSTRAP: // 初始化
+        case AppStatus.BOOTSTRAPPED: // 启动
+        case AppStatus.UNMOUNTED: // 卸载
           result.push(app)
           break
       }
+      // 如果已经处于 mount 状态并且 activeRule() 返回值为 true， result 返回空 不作任何处理。
     }
     // 卸载
+    // isActive(app)为false 不是当前路由且 状态为MOUNTED
     else if (app.status === AppStatus.MOUNTED && status === AppStatus.MOUNTED) {
       result.push(app)
     }
