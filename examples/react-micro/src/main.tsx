9934edea450650e68f97ebb732b93683da81e43b
plumliil_li <plumliil.li@elitesland.com>
2023-09-27 22:39:49 +0800

    feat: react micro option update in version 2.

--- examples/react-micro/src/main.tsx
+++ examples/react-micro/src/main.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function render(options: any) {
+  const { container } = options
+  console.log('react container', container)
+
+  ReactDOM.render(
+    <App />,
+    container
+      ? document.querySelector('#microapp')
+      : document.querySelector('#root')
+  )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function bootstrap(props: any) {
+  console.log(' react micro bootstrap ', props)
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function mount(options: any) {
+  console.log(' react micro mount options', options)
+  render(options)
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function unmount(options: any) {
+  const { container } = options
+  console.log(' react micro unmount ')
+  ReactDOM.unmountComponentAtNode(
+    container ? document.querySelector('#microapp') : document.querySelector('#root')
+  )
+}
+
+if (window['__IS_SINGLE_SPA__']) {
+  window[`mini-single-spa-react`] = {
+    bootstrap,
+    mount,
+    unmount,
+  }
+} else {
+  render({})
+}
